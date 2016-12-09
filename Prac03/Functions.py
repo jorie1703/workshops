c3662ae968d98986a22e78ea58f495a5c3248ad2
jc226070 <jorie.gomez@my.jcu.edu.au>
2016-12-09 20:45:47 +1000

    1st Function weekly practical for Wk4

---
+++ Prac03/Functions.py
@@ -0,0 +1,16 @@
+__author__ = 'jc226070'
+"""
+"""
+text = "Merry Christmas"
+print(text[:5] + text[6:12])
+
+
+
+
+
+def inch_to_meter(inch):
+   return inch * 0.0254
+
+print(inch_to_meter(5))
+
+
